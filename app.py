6540d2277f8387cea1b4260bb34679609823dc62
Gauri Santhosh <122504552+gauri-santhosh@users.noreply.github.com>
2024-05-17 20:16:46 +0530

    latest

--- app.py
+++ app.py
@@ -148,5 +148,5 @@ def booking():
     else:
         return render_template('booking.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '_main_':
+    app.run(host="0.0.0.0", port=5000)
